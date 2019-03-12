796290f7ea4dc8e253b1f74ca6f9a2b0e8de9cd5
ShirleyQ <quintanashirl@gmail.com>
2019-03-11 18:19:33 -0600

    count of months

--- 03_Python/PyBank.py
+++ 03_Python/PyBank.py
@@ -14,18 +14,20 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     # identify header row
-    header = next(csvreader)
+   # header = next(csvreader)
+    monthList =[]
     
+    #identify header row
+    header = next(csvreader)
+
     # count number of months
     for row in csvreader:
-      
-      data = list(csvreader)
-      months = int(len(data))
-    
-    #------------- the month count is 85 not 86 - need to figure out why--------
-    print("Financial Analysis")
-    print("--------------------------")
-    print("Total Months:") #need to get # of months to print here
+     
+      monthList.append(str(row[0]))
+
+    print(str(len(monthList)))
+
+     
 
 #--------------------------------------------------------------------------------------------
 # 2. average the profit loss column
